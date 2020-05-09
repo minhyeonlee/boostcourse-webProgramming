f40e9d24f58518c37a17f5c3073043a716437568
Minhyeon Lee <minhyeonlee1@gmail.com>
2020-05-09 21:34:17 +0900

    4-3. Spring IoC practice with ApplicationContext

---
+++ diexam01/src/main/java/kr/or/connect/diexam01/UserBean.java
@@ -0,0 +1,50 @@
+package kr.or.connect.diexam01;
+
+public class UserBean {
+
+/* Bean 규칙(생성을 사용자가 하는 것이 아니라 위임한다.)
+ * 1) 기본생성자를 가지고 있다.
+ * 2) 필드는 private으로 선언한다.
+ * 3) getter, setter 메소드를 가진다. getName(), setName() 메소드를 name 프로퍼디(property)라고 한다.
+ */
+	
+	private String name;
+	private int age;
+	private boolean male;
+	
+	public UserBean() {
+		
+	}
+
+	public UserBean(String name, int age, boolean male) {
+		this.name = name;
+		this.age = age;
+		this.male = male;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public boolean isMale() {
+		return male;
+	}
+
+	public void setMale(boolean male) {
+		this.male = male;
+	}
+	
+	
+}
